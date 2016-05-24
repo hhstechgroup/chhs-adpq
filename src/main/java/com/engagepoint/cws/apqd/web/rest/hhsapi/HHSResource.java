ef7d236b185e977a4c354c361736bee1b1de662d
dmitry.rudenko <dmitry.rudenko@engagepoint.com>
2016-05-24 16:35:18 -0400

    APQD-2: HHS API Service

--- src/main/java/com/engagepoint/cws/apqd/web/rest/hhsapi/HHSResource.java
+++ src/main/java/com/engagepoint/cws/apqd/web/rest/hhsapi/HHSResource.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.net.URISyntaxException;
 
 /**
  * Created by dmitry.rudenko on 5/20/2016.
@@ -22,14 +24,14 @@ public class HHSResource {
         method = RequestMethod.POST, produces = "application/json")
     @Timed
     @ResponseBody
-    public String findFosterFamilyAgencies(@RequestBody Box box) throws Exception {
+    public String findFosterFamilyAgencies(@RequestBody Box box) throws IOException, URISyntaxException {
         return hhsService.findFosterFamilyAgencies(box.getNorthwest().getLatitude(), box.getNorthwest().getLongitude(),
             box.getSoutheast().getLatitude(), box.getSoutheast().getLongitude());
     }
 
     @RequestMapping(value = "/hhs/fosterFamilyAgencies.json",
         method = RequestMethod.GET, produces = "application/json")
-    public String findFosterFamilyAgencies() throws Exception {
+    public String findFosterFamilyAgencies() throws IOException, URISyntaxException {
         String queryString = request.getQueryString();
         return hhsService.findFosterFamilyAgencies(queryString);
     }
