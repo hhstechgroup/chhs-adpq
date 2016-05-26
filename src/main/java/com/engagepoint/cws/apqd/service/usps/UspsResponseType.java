469df96b8f4943919f97198a5a18da13f2b9ddbb
Sergii Redchuk <serge.redchuk@engagepoint.com>
2016-05-26 14:11:28 -0400

    removed unused USPS address validation functionality

--- src/main/java/com/engagepoint/cws/apqd/service/usps/UspsResponseType.java
+++
@@ -1,8 +0,0 @@
-package com.engagepoint.cws.apqd.service.usps;
-
-/**
- * Created by dmytro.palczewski on 2/11/2016.
- */
-public enum UspsResponseType {
-    SUCCESS, ERROR
-}
--- src/main/webapp/scripts/components/usps/usps.service.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('apqdApp')
-    .factory('USPS', function ($resource) {
-        return $resource('usps/verify-address', {}, {
-            'verifyAddress': { method:'PUT' }
-        });
-    });
