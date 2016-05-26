2f3dbef5b7681d29ebcf9e89f33d9cac3df5ae37
oleg.korneychuk <oleg.korneychuk@engagepoint.com>
2016-05-26 09:04:26 -0400

    APQD-38 Use leaflet directive. Implement locations refreshment approach

--- src/main/webapp/scripts/app/POC/locations/locations.controller.js
+++ src/main/webapp/scripts/app/POC/locations/locations.controller.js
@@ -2,69 +2,67 @@
 
 angular.module('apqdApp')
     .controller('LocationsController', ['$scope', '$log', '$q', 'FosterFamilyAgenciesService', function ($scope, $log, $q, FosterFamilyAgenciesService) {
-        $scope.CENTER_ID = 'current';
-        $scope.currentLocation;
-
-        if (navigator.geolocation) {
-            $log.debug('Geolocation is supported!');
-            getCurrentLocation().then(function(marker) {
-                $scope.currentLocation = marker;
-                $scope.markers.push(marker);
-            }, $log.error);
-        } else {
-            $log.warn('Geolocation is not supported for this Browser/OS version yet.');
-        }
-
-        //todo: mock data
-        function createMarkers() {
-            return [
-                {id:1, latitude: 39.04, longitude: -76.92, options: {visible: true}},
-                {id:2, latitude: 39.05, longitude: -76.93, options: {visible: true}},
-                {id:3, latitude: 39.06, longitude: -76.94, options: {visible: true}}
-            ];
-        }
-
-        function getCurrentLocation() {
-            var q = $q.defer();
-            var geoSuccess = function(position) {
-                var marker = createMarker(position.coords.latitude, position.coords.longitude, $scope.CENTER_ID);
-                q.resolve(marker);
-            };
-            navigator.geolocation.getCurrentPosition(geoSuccess, q.reject);
-            return q.promise;
-        }
-
-        function createMarker(latitude, longitude, id) {
-            if (!id) {
-                id = _.random();
-            }
-            return {
-                id: id,
-                latitude: latitude,
-                longitude: longitude,
-                options: {
-                    visible: true
-                }
-            };
-        }
-
-        $scope.updateLocations = function(bounds) {
-            $log.debug(bounds);
-            $scope.markers.length = 0;
-            Array.prototype.push.apply($scope.markers, createMarkers());
-            $scope.markers.push($scope.currentLocation);
-
-            var request = {
-                "northwest": {
-                    "latitude": 34.185175,
-                    "longitude": -117.77147
-                },
-                "southeast": {
-                    "latitude": 34.075175,
-                    "longitude": -117.57147
-                }
-            }
+        $scope.defaults = {
+            tileLayer: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+            maxZoom: 18
         };
+
+        $scope.center = {autoDiscover: true, zoom: 13};
+        $scope.$watch('center.autoDiscover', function(newValue) {
+            if (!newValue) {
+                $scope.currentLocation = {
+                    lat: $scope.center.lat,
+                    lng: $scope.center.lng,
+                    focus: true,
+                    message: 'You are here'
+                };
+                $scope.updateLocations();
+
+                $scope.$watch('center.autoDiscover');
+            }
+        });
+
+        $scope.updateLocations = function() {
+            leafletData.getMap().then(function (map) {
+                $log.debug(map.getBounds());
+
+                $scope.locations = {
+                    l1: {
+                        lat: 39.04,
+                        lng: -76.92,
+                        message: 'Location 1'
+                    },
+                    l2: {
+                        lat: 39.05,
+                        lng: -76.93,
+                        message: 'Location 2'
+                    },
+                    l3: {
+                        lat: 39.06,
+                        lng: -76.9386394,
+                        message: 'Location 3'
+                    }
+                };
+                if ($scope.currentLocation) {
+                    $scope.locations.current = $scope.currentLocation;
+                }
+            });
+        };
+
+        $scope.$on("leafletDirectiveMap.viewreset", function(event, args) {
+            $log.debug(event.name);
+            $scope.updateLocations();
+        });
+
+        $scope.$on("leafletDirectiveMap.dragend", function(event, args) {
+            $log.debug(event.name);
+            $scope.updateLocations();
+        });
+
+        $scope.$on("leafletDirectiveMap.resize", function(event, args) {
+            $log.debug(event.name);
+            $scope.updateLocations();
+        });
 
         //TODO: sample call
         $scope.test = function () {
