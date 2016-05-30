90b7136bfe236cb83f0a08514b8bb2b60804eef0
Sergii Redchuk <serge.redchuk@engagepoint.com>
2016-05-30 13:51:25 -0400

    APQD-31: Cover server side Java code with unit tests to reach acceptable 65% of coverage

---
+++ src/test/java/com/engagepoint/cws/apqd/service/util/RandomUtilTest.java
@@ -0,0 +1,32 @@
+package com.engagepoint.cws.apqd.service.util;
+
+import org.junit.Test;
+
+import static com.engagepoint.cws.apqd.service.util.RandomUtil.generateActivationKey;
+import static com.engagepoint.cws.apqd.service.util.RandomUtil.generatePassword;
+import static com.engagepoint.cws.apqd.service.util.RandomUtil.generateResetKey;
+import static org.assertj.core.api.StrictAssertions.assertThat;
+
+public class RandomUtilTest {
+
+    @Test
+    public void testGeneratePassword() throws Exception {
+        final String password1 = generatePassword();
+        assertThat(password1).isNotNull();
+        assertThat(password1).isNotEqualTo(generatePassword());
+    }
+
+    @Test
+    public void testGenerateActivationKey() throws Exception {
+        final String activationKey1 = generateActivationKey();
+        assertThat(activationKey1).isNotNull();
+        assertThat(activationKey1).isNotEqualTo(generateActivationKey());
+    }
+
+    @Test
+    public void testGenerateResetKey() throws Exception {
+        final String resetKey = generateResetKey();
+        assertThat(resetKey).isNotNull();
+        assertThat(resetKey).isNotEqualTo(generateResetKey());
+    }
+}
