cdc14c7ed5eb11bc266c428e7b2dd54ad45abe80
dmitry.rudenko <dmitry.rudenko@engagepoint.com>
2016-05-31 16:50:41 -0400

    APQD-39: default impl

--- src/main/webapp/scripts/app/account/settings/settings.controller.js
+++ src/main/webapp/scripts/app/account/settings/settings.controller.js
@@ -3,7 +3,7 @@
 angular.module('apqdApp')
     .controller('SettingsController',
     function ($scope, Principal, Auth, Language, $translate, uibCustomDatepickerConfig, DateUtils, lookupGender,
-     Place) {
+     Place, leafletData) {
         $scope.dateOptions = uibCustomDatepickerConfig;
         $scope.success = null;
         $scope.error = null;
@@ -49,4 +49,44 @@ angular.module('apqdApp')
         };
 
         $scope.lookupGender = lookupGender;
+
+        $scope.addGeocoder = function () {
+            if($scope.geocoder) {
+                return;
+            }
+            $scope.geocoder = L.control.geocoder('search-JrJjTaE', {
+                fullWidth: 650,
+                expanded: true,
+                markers: true,
+                latlng: true,
+                place : true
+            });
+            $scope.geocoder.on('select', function (e) {
+                $scope.onSelectAddress(e);
+            });
+            leafletData.getMap().then(function (map) {
+                $scope.geocoder.addTo(map);
+                document.getElementById("geocoder").appendChild($scope.geocoder._container);
+            });
+        };
+
+
+        //See geojson
+        $scope.onSelectAddress = function (addressFeature) {
+            $scope.fillAddress(
+                addressFeature.feature.properties.name,
+                addressFeature.feature.properties.locality,
+                addressFeature.feature.properties.region_a,
+                ""
+            );
+        };
+
+        $scope.fillAddress = function (street, city, state, zip) {
+            $scope.street = street;
+            $scope.city = city;
+            $scope.state = state;
+            $scope.zip = zip;
+        };
+
+        $scope.addGeocoder();
     });
