3a1014fcfeea1031d18ada81387dca36c264734e
dmitry.rudenko <dmitry.rudenko@engagepoint.com>
2016-06-05 01:53:42 -0400

    APQD-148: init twitter timeline

--- src/main/webapp/scripts/app/account/login/login.controller.js
+++ src/main/webapp/scripts/app/account/login/login.controller.js
@@ -32,4 +32,14 @@ angular.module('apqdApp')
             return AuthenticationErrorService.getAuthenticationError();
         };
 
+        $scope.initTwitterTimeline = function () {
+            var twitterJs = document.getElementById("twitter-wjs");
+            if(twitterJs) {
+                twitterJs.parentNode.removeChild(twitterJs);
+            }
+            !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+        };
+
+
+        $scope.initTwitterTimeline();
     });
