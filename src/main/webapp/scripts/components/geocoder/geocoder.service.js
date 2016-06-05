89ed7dc1c6d786815689fd858cf878abccf8e2ad
dmitry.rudenko <dmitry.rudenko@engagepoint.com>
2016-06-05 10:22:55 -0400

    APQD-102

--- src/main/webapp/scripts/components/geocoder/geocoder.service.js
+++ src/main/webapp/scripts/components/geocoder/geocoder.service.js
@@ -1,6 +1,6 @@
 angular.module('apqdApp')
     .factory('GeocoderService', ['leafletData', '$q', function (leafletData, $q) {
-        var getDistanceFromLatLonInKm = function (lat1,lon1,lat2,lon2) {
+        var getDistance = function (lat1,lon1,lat2,lon2) {
             var R = 6371 * .621371; // Radius of the earth in miles
             var dLat = deg2rad(lat2-lat1);  // deg2rad below
             var dLon = deg2rad(lon2-lon1);
@@ -42,7 +42,7 @@ angular.module('apqdApp')
 
             distance : function (location1, location2) {
                 var deferred = $q.defer();
-                var result = getDistanceFromLatLonInKm(location1.latitude, location1.longitude, location2.latitude, location2.longitude);
+                var result = getDistance(location1.latitude, location1.longitude, location2.latitude, location2.longitude);
                 deferred.resolve(result);
                 return deferred.promise;
             }
