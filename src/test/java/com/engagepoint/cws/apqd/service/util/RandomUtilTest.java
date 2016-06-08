3a52e01a228d0cd3c583681ccba2d1cf2092c5d3
Sergii Redchuk <serge.redchuk@engagepoint.com>
2016-06-08 19:07:15 -0400

    ADPQ-12: Fix Sonar issues in the template application code

--- src/test/java/com/engagepoint/cws/apqd/service/util/RandomUtilTest.java
+++ src/test/java/com/engagepoint/cws/apqd/service/util/RandomUtilTest.java
@@ -2,12 +2,18 @@ package com.engagepoint.cws.apqd.service.util;
 
 import org.junit.Test;
 
+import static com.engagepoint.cws.apqd.APQDTestUtil.assertThatConstructorIsPrivate;
 import static com.engagepoint.cws.apqd.service.util.RandomUtil.generateActivationKey;
 import static com.engagepoint.cws.apqd.service.util.RandomUtil.generatePassword;
 import static com.engagepoint.cws.apqd.service.util.RandomUtil.generateResetKey;
 import static org.assertj.core.api.StrictAssertions.assertThat;
 
 public class RandomUtilTest {
+
+    @Test
+    public void testConstructorIsPrivate() throws Exception {
+        assertThatConstructorIsPrivate(RandomUtil.class);
+    }
 
     @Test
     public void testGeneratePassword() throws Exception {
